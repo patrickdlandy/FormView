89e04988fa1841ebe2701d41c138ae966dd5210d
Patrick Landy <patrickdlandy@gmail.com>
2019-10-02 19:30:45 -0400

    Create users and session reducers.

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,57 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveCurrentUser = function(user) {
+  return({
+    type: RECEIVE_CURRENT_USER,
+    user: user
+  });
+}
+
+export const logoutCurrentUser = function() {
+  return({
+    type: LOGOUT_CURRENT_USER
+  });
+};
+
+export const receiveErrors = function(errors) {
+  return({
+    type: RECEIVE_ERRORS,
+    errors: errors
+  })
+}
+
+//THUNKS
+
+export const login = function(user) {
+  return function(dispatch) {
+    return SessionApiUtil.login(user).then(
+      function(payload) {
+        dispatch(receiveCurrentUser(payload));
+      }
+    );
+  }
+}
+
+export const logout = function() {
+  return function(dispatch) {
+    return SessionApiUtil.logout().then(
+      function() {
+        dispatch(logoutCurrentUser());
+      }
+    );
+  }
+}
+
+export const signup = function(user) {
+  return function(dispatch) {
+    return SessionApiUtil.signup(user).then(
+      function(payload) {
+        dispatch(receiveCurrentUser(payload));
+      }
+    );
+  }
+}
