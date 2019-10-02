c6ec8c16563bf56e41218b36777e5d58d2a52861
Patrick Landy <patrickdlandy@gmail.com>
2019-10-02 10:58:04 -0400

    Create api users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
 
-  resources :users, only: [:create]
+  resources :users, only: [:index, :create]
 end
