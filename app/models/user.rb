5e784d57be7874249a458e3a36c7a83472dc049d
Patrick Landy <patrickdlandy@gmail.com>
2019-10-02 11:55:50 -0400

    Add FIGVAPER and CELL methods to user model and application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,45 @@
 #  updated_at      :datetime         not null
 #
 
-
 class User < ApplicationRecord
+
+  #figvaper
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :session_token, presence: true
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    if user.is_password?(password) 
+      return user
+    else
+      return nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
 
 end
