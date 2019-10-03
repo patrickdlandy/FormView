44f7fa0172d65fb89d659aa08fde5cb020ad5020
Patrick Landy <patrickdlandy@gmail.com>
2019-10-03 13:56:03 -0400

    Finish greeting container

--- frontend/components/greeting/greeting_container.jsx
+++ frontend/components/greeting/greeting_container.jsx
@@ -1,15 +1,17 @@
 import { connect } from 'redux';
 import Greeting from './greeting';
+import { logout } from '../../actions/session_actions'
 
-
-export const mapStateToProps = function(state) {
+export const mapStateToProps = function({ session, entities: { users } }) {
   return {(
-
+    currentUser: users[session.id]
   )}
 }
 
 export const mapDispatchToProps = function(dispatch) {
   return {(
-
+    logout: () => dispatch(logout())
   )}
 }
+
+export default connect(mapDispatchToProps, mapStateToProps)(Greeting);
