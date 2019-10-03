3a3645401925de1c56e625e9885781c7f7748ddc
Patrick Landy <patrickdlandy@gmail.com>
2019-10-02 20:51:48 -0400

    Add store and window test functions

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 import  { login, logout, signup } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -12,6 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
   window.login = login;
   window.logout = logout;
   window.signup = signup;
+
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   ReactDOM.render(<h1>FORMVIEW</h1>, document.getElementById('root'));
 });
