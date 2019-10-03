a807c948ba6d80b844ae7cf3192f6e7b5382d7ae
Patrick Landy <patrickdlandy@gmail.com>
2019-10-03 10:37:20 -0400

    Begin Greeting components.

---
+++ frontend/components/greeting/greeting_container.jsx
@@ -0,0 +1,15 @@
+import { connect } from 'redux';
+import Greeting from './greeting';
+
+
+export const mapStateToProps = function(state) {
+  return {(
+
+  )}
+}
+
+export const mapDispatchToProps = function(dispatch) {
+  return {(
+
+  )}
+}
