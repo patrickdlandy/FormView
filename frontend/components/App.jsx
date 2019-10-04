c188ce768de94615a398bcfade45182ef5fee9f5
Patrick Landy <patrickdlandy@gmail.com>
2019-10-03 21:54:46 -0400

    Complete and test login/logout functionality

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
 import GreetingContainer from "./greeting/greeting_container";
-
+import LoginFormContainer from "./session_form/login_form_container";
+import SignupFormContainer from "./session_form/signup_form_container";
 const App = () => (
   <div>
+    <header>
     <h1>FormView!</h1>
     <GreetingContainer />
+    </header>
+ 
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
   </div>
 );
 
