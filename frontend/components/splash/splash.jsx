51d3fd2f0e1421a71344c92a5e4ac4a1bc42459e
Patrick Landy <patrickdlandy@gmail.com>
2019-10-24 09:55:46 -0400

    Add credits on splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -143,6 +143,9 @@ class Splash extends React.Component {
                         </div>
                     </div>
                     <div className="credits-container">
+                        <p>FormView design and functionality inspired by <a href="https://www.wufoo.com/">Wufoo</a></p>
+                            <p>Wufoo legacy styling is archived <a href="https://web.archive.org/web/20191012012529/https://www.wufoo.com/"></a>here</p>
+                        <p>Glyptodon artwork by Rachel Margolis</p>
                         <p>Arrow icon made by <a href="https://www.flaticon.com/authors/lyolya" title="Lyolya">Lyolya</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
                     </div>
                 </div>
@@ -194,88 +197,5 @@ class Splash extends React.Component {
         }
     }
 }
-
-//Old splash (pre-react component)
-
-// const renderForms = function(props) {
-    //props.fetchForms();
-    // return(
-    //     <div>
-    //         {props.forms.map( function(form, i){
-    //             return (
-    //                 <li className="" key={`form-${i}`}>
-    //                     {form.name}
-    //                 </li>
-    //             )
-    //         })}
-    //     </div>
-    // );
-// }
-
-// const Splash = function(props) {
-//     if (!props.currentUser) {
-//         return(
-//             <div>
-//             <nav className="main-nav-container">
-//                 <h1 className="nav-element">FormView by Patrick Landy</h1>
-//                 <ul className="nav-ul">
-//                     <li className="nav-element">
-//                         <a href="https://github.com/patrickdlandy" className="nav-link" target="_blank">GITHUB</a>
-//                     </li>
-//                     <li className="nav-element">
-//                         <a href="https://www.linkedin.com/in/patrick-landy-pe-cphc-178a279a/" className="nav-link" target="_blank">LINKEDIN</a>
-//                     </li>
-//                 </ul> 
-//                 <nav className="signup-btn-container">
-//                     <Link to="/signup" className="signup-btn">SIGN UP</Link>
-//                 </nav>
-//                 <Link to="/login" 
-//                 className="login-link" 
-//                 data-hover-replace="GRONK!"
-//                 data-original-text="LOGIN"
-//                 >LOGIN</Link>
-//             </nav>
-//             <main className="main-container">
-//                 <div>
-//                     <h1 className="general-main-header">Build and share online forms</h1>
-//                     <div>
-//                         <Link to="/login">Login</Link>
-//                         <br/>
-//                         <Link to="/signup">Sign Up!</Link>
-//                     </div>
-//                 </div>
-//             </main>
-//             <div className="bottom-container">
-//                 <div>
-//                     <h2 className="general-sub-header">Building online forms can be hard. </h2>
-//                     <h3 className="general-sub-header">FormView makes it easy. </h3>
-//                 </div>
-//             </div>
-//             </div>
-//         )
-//     } else {
-//         return(
-//             <div>
-//                 <nav className="main-nav-container">
-//                     <h1 className="nav-element">FormView by Patrick Landy</h1>
-//                     <ul className="nav-ul">
-//                         <li className="nav-element">
-//                             <a href="https://github.com/patrickdlandy" className="nav-link" target="_blank">GITHUB</a>
-//                         </li>
-//                         <li className="nav-element">
-//                             <a href="https://www.linkedin.com/in/patrick-landy-pe-cphc-178a279a/" className="nav-link" target="_blank">LINKEDIN</a>
-//                         </li>
-//                     </ul>
-//                 </nav>
-//                 <main className="bottom-container">
-//                     <div>
-//                         <h1 className="general-sub-header">Form Manager</h1>
-//                         {renderForms(props)}
-//                     </div>
-//                 </main>
-//             </div>
-//         );
-//     }
-// }
 
 export default withRouter(Splash);
