375539e52667e68565e95aaec1da9da167e2aea4
Patrick Landy <patrickdlandy@gmail.com>
2019-11-30 16:24:29 -0500

    Build and test successful component state management for option selections

--- frontend/components/form_show/form_show.jsx
+++ frontend/components/form_show/form_show.jsx
@@ -10,6 +10,9 @@ class FormShow extends React.Component {
         this.renderOptions = this.renderOptions.bind(this);
         this.contentChange = this.contentChange.bind(this);
         this.handleRadioSelection = this.handleRadioSelection.bind(this);
+        this.responseChange = this.responseChange.bind(this);
+        this.elementsToState = this.elementsToState.bind(this);
+        this.getResponses = this.getResponses.bind(this);
         this.state = {
             menuDisplayed: false,
             elementsLoaded: false,
@@ -24,16 +27,41 @@ class FormShow extends React.Component {
             });
     }
 
+    responseChange(elementId, optionId) {
+        // console.log(this.state.responses);
+        this.setState(function(prevState) {
+            return Object.assign({}, prevState, { responses: {
+                ...prevState.responses,
+                [elementId]: optionId
+            }})
+        }, function () {
+            console.log(this.state.responses);
+        });
+    }
+
+    elementsToState() {
+        const localResponseChange = this.responseChange;
+        Object.keys(this.props.elements).forEach(function(ele) {
+            localResponseChange(ele, null);
+        });
+        // console.log(this.state.responses);
+    }
+
     componentDidMount() {
         const contentChange = this.contentChange;
+        const localElementsToState = this.elementsToState;
         this.props.fetchForm(this.props.formId);
         this.props.fetchElements().then(function () {
-            contentChange("elementsLoaded")
+            contentChange("elementsLoaded");
+            localElementsToState();
         });
-        // console.log(this.state.elementsLoaded);
         this.props.fetchOptions().then(function() {
             contentChange("optionsLoaded")
         });
+    }
+
+    getResponses() {
+        return this.state.responses;
     }
 
     componentWillUnmount() {
@@ -42,7 +70,6 @@ class FormShow extends React.Component {
     }
 
     renderForm() {
-        //Getting the elements and options might make sense here instead of in nested functions.
         if (this.props.form && this.state.optionsLoaded === true && this.state.elementsLoaded === true ) {
             return(
                 <div className="form-show-container">
@@ -74,7 +101,9 @@ class FormShow extends React.Component {
         const local_elements = elements;
         const local_options = options;
         const local_form = this.props.form;
+        const local_responses = this.state.responses;
         const localHandleRadioSelection = this.handleRadioSelection;
+        const localGetResponses = this.getResponses;
         if (this.props.form) {
             if (this.props.form.element_ids.length === 0) {
                 return (
@@ -109,7 +138,13 @@ class FormShow extends React.Component {
                         return(
                             <div key={id}>
                                 <span>
-                                    <input type="radio" className="radio-button" name={id1} onChange={localHandleRadioSelection(id1, id)}/>
+                                    <input 
+                                    type="radio" 
+                                    className="radio-button" 
+                                    name={id1} 
+                                    checked={localGetResponses()[id1] === id.toString()} 
+                                    value={id}
+                                    onChange={localHandleRadioSelection(id1, id)} />
                                     <label> 
                                         {" " + local_options[id].title + ": " + local_options[id].body}
                                     </label>
@@ -141,17 +176,25 @@ class FormShow extends React.Component {
         });
     }
 
-    updateSelection(questionId, optionId) {
-        this.setState({
-            responses: {
-                [questionId]: optionId
-            }
-        })
-        console.log(optionId);
-    }
+    // updateSelection(questionId, optionId) {
+    //     this.setState(function() {
+    //         return({
+    //         responses: {
+    //             [questionId]: optionId
+    //         }});
+    //     }, function() {
+    //         console.log(this.state.responses);
+    //     });
+    //     console.log(questionId);
+    //     console.log(optionId);
+    // }
 
     handleRadioSelection(questionId, optionId) {
-        this.updateSelection(questionId, optionId);
+        // const localUpdateSelection = this.updateSelection;
+        const localResponseChange = this.responseChange;
+        return function(e) {
+            localResponseChange(questionId, e.target.value);
+        }
     }
 
     render() {
